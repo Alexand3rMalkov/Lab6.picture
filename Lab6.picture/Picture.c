46271e06446ed3c911e6c019026773177d8c7c96
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2022-10-25 21:11:26 +0300

    Update Picture.c

--- Lab6.picture/Picture.c
+++ Lab6.picture/Picture.c
@@ -3,25 +3,29 @@
 #include <locale.h>
 #include <math.h>
 
-int figure(int x, int y);
+int fig(int x, int y);
 void main()
 {
 	int x, y, X, Y;
 	setlocale(LC_ALL, "rus");
+	printf("Введите X:");
 	scanf("%d", &X);
+	printf("Введите Y:");
 	scanf("%d", &Y);
-
+	printf("Требуемая фигура:");
+	printf("\n");
     for (x = -(X / 2); x <= X / 2; ++x)
     {
         for (y = -(Y / 2); y <= Y / 2; ++y)
         {
-			if (figure(x, y)) printf("* ");
+			if (fig(x, y)) printf("* ");
 			else printf(" ");
 		}
 		printf("\n");
 	}
 }
-int figure(int x, int y)
+
+int fig(int x, int y)
 {
 
     if ((x <= 0 && y >= 0) && (x - y <= 3) && ((pow(x, 2) + pow(y, 2)) <= 36)) return 1;
