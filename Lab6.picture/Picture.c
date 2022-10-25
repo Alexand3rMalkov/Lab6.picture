d79a528e8c262e5d404129eb950d12bbc153ba31
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2022-10-25 20:58:34 +0300

    Добавьте файлы проекта.

---
+++ Lab6.picture/Picture.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int figure(int x, int y);
+void main()
+{
+	int x, y, X, Y;
+	setlocale(LC_ALL, "rus");
+	scanf("%d", &X);
+	scanf("%d", &Y);
+
+    for (x = -(X / 2); x <= X / 2; ++x)
+    {
+        for (y = -(Y / 2); y <= Y / 2; ++y)
+        {
+			if (figure(x, y)) printf("x ");
+			else printf("* ");
+		}
+		printf("\n");
+	}
+
+}
+int figure(int x, int y)
+{
+
+    if ((x <= 0 && y >= 0) && (x - y <= 3) && ((pow(x, 2) + pow(y, 2)) <= 36)) return 1;
+    //if ((x >= 0 && y >= 0) && (-x + 3 <= y) && ((pow(x, 2) + pow(y, 2)) <= 36))  return 1;
+	else return 0;
+}
